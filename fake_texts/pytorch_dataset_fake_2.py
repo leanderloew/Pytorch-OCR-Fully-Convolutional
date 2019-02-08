7497e815a794fae0e5b08bb3e37dcd5e02c9aae4
leander <leander.low@outlook.de>
2019-02-08 11:56:44 +0100

    wrote better generator that generates during get_item also with words spaced appart

--- fake_texts/pytorch_dataset_fake_2.py
+++ fake_texts/pytorch_dataset_fake_2.py
@@ -4,6 +4,7 @@ sys.path.append("fake_texts/")
 import argparse
 import os, errno
 import random
+from random import randint
 import string
 from tqdm import tqdm
 
@@ -26,6 +27,7 @@ from imgaug import augmenters as iaa
 from imgaug_transformations import augmentations
 from skimage.transform import resize
 import cv2
+
 
 
 #Basically we want to add the
@@ -61,63 +63,25 @@ from torch.utils import data
 
 class Dataset(data.Dataset):
     'Characterizes a dataset for PyTorch'
-    def __init__(self,batch_size,epoch_size=10,random_strings=True,num_words=5,transform=False,width=-1,alignment=1,height=32):
+    def __init__(self,epoch_size=10,random_strings=True,num_words=5,transform=False,width=-1,alignment=1,height=32):
         'Initialization'
-        #General args:
-        self.transform=transform
-        self.random_strings=random_strings
-        self.num_words=num_words
-        #How much data we want to generate in a single epoch 
-        self.epoch_size=epoch_size
-        self.batch_size=batch_size
-
-        def take_10(x):
-
-            if len(x)>self.num_words:
-                return x[0:self.num_words]
-            else:
-                return x 
-
-        #Text gen specific stuff 
-        self.thread_count=6
-        self.language="en"
-        self.count=32
-        #If we want to use random seq, alternatively we use Wikipedia ( needs internet)
+        #Still in inti
+        self.transform=True
         self.random_sequences=False
+        self.random_strings=True
         self.include_letters=True
         self.include_numbers=True
         self.include_symbols=True
-        #When source wikipedia, how many words we want to include
         self.length=10
-        #If we want to have variable word lengths, "length is maximum)
-        self.random=True
-        #The height of the image
-        self.format=height
-        #Skeqing angle
-        self.skew_angle=0
-        self.random_skew=True
-        self.use_wikipedia=True
-        self.blur=0
-        self.random_blur=True
-
-        """Define what kind of background to use. 0: Gaussian Noise, 1: Plain white, 2: Quasicrystal, 3: Pictures""",
-        self.background=1
-
-        """Define a distorsion applied to the resulting image. 0: None (Default), 1: Sine wave, 2: Cosine wave, 3: Random""",
-
-        self.distorsion=0
-        """Define the distorsion's orientation. Only used if -d is specified. 0: Vertical (Up and down), 1: Horizontal (Left and Right), 2: Both"""
-        self.distorsion_orientation=0
-        self.width=width
-        self.orientation=0
+        #self.random=False
+        self.format=32
+        self.use_wikipedia=False
         self.text_color='#282828'
-        self.space_width=0.1
+        self.orientation=0
         self.extension="jpg"
         self.handwritten=False
         self.name_format=0
-        self.alignment=alignment
 
-        #This shoule be done on init We also do string generation in the init of the dataset
         pool = ''
         pool += "abcdefghijklmnopqrstuvwxyz"
         pool += "0123456789"
@@ -127,89 +91,100 @@ class Dataset(data.Dataset):
         self.values = np.array(range(1,len(pool)+1))
         self.dictionary = dict(zip(self.keys, self.values))
         self.fonts = load_fonts("en")
-        
+
         self.decode_dict=dict((v,k) for k,v in self.dictionary.items())
         self.decode_dict.update({67 : "OOK"})
+        self.seq = augmentations
 
-        ###Get strings
+    def __len__(self):
+        'Denotes the total number of samples'
+        return 10000
+    def transform(self,img):
+        return self.seq.augment_images(img)
+
+    def __getitem__(self, index):
+        #We are actually only getting one item here obvoiusly!
+
+
+        num_words=randint(5,6)
+        language="en"
+        count=32
+        skew_angle=0
+        random_skew=True
+        blur=0
+        random_blur=True
+        background=randint(0,1)
+        distorsion=randint(0,2)
+        distorsion_orientation=randint(0,2)
+
+        #as class function
+        def take_10(x):
+
+            if len(x)>num_words:
+                return x[0:num_words]
+            else:
+                return x 
+
+        #if random.random()>0.8:
+        #    self.width=random.randint(500,800)
+        #else:
+        width=-1
+
+        alignment=random.randint(0,2)
+
         strings = []
-        #We try to load strings from wikipedia, if not we return random strings. 
-        if self.random_strings==False:
-            try:
-                #Num words just doesnt work here I think it takes one sentence always
-                strings = create_strings_from_wikipedia(1, self.batch_size*self.epoch_size, "en")
-            except:
-                print("Connection issues")
-                strings = create_strings_randomly(self.num_words, False, batch_size*epoch_size,
-                                          True, True,True, "en")
-        else:
-            strings = create_strings_randomly(self.num_words, False, batch_size*epoch_size,
-                          True, True,True, "en")
+        strings = create_strings_randomly(num_words, False, 1,
+                      True, True,True, "en")
 
-        ###Get Images 
-        #Here we actually take up to n words, by word tokenizing and then taking n 
         strings =[" ".join(take_10(word_tokenize(x))) for x in strings]
-        #Next we split into cahracter
+
         strings_=[list(x)for x in strings]
-        #self.strings=strings
-        #self.strings_=strings_
-        #Then we convert to interger, 93 for symbols we dont know
         self.strings_int=[[self.dictionary[x.lower() ] if x.lower()  in self.keys else 67 for x in m ] for m in strings_]
-        #Then we get the lengths, we need for loss
         self.strings_len=[len(x)for x in self.strings_int]
         string_count = len(strings)
-        #We can write it in a neat list comprehension, enough optimization for me haha 
-        self.image_list=[np.expand_dims(np.array(FakeTextDataGenerator.generate(*j)),0) for j in zip(
+
+        #What we do here is we space the words quite far appart. 
+        if random.random()>0.5:
+            width_scale=random.random()*900
+            space_width=width_scale/np.sum(self.strings_len)
+        else:
+            width_scale=random.random()
+            space_width=width_scale/np.sum(self.strings_len)
+            if random.random()>0.85 and np.max(self.strings_len)<30:
+                width=random.randint(500,800)
+
+        image_list=[np.expand_dims(np.array(FakeTextDataGenerator.generate(*j)),0) for j in zip(
                     [i for i in range(0, string_count)],
                     strings,
                     [self.fonts[random.randrange(0, len(self.fonts))] for _ in range(0, string_count)],
                     [self.format] * string_count,
                     [self.extension] * string_count,
-                    [self.skew_angle] * string_count,
-                    [self.random_skew] * string_count,
-                    [self.blur] * string_count,
-                    [self.random_blur] * string_count,
-                    [self.background] * string_count,
-                    [self.distorsion] * string_count,
-                    [self.distorsion_orientation] * string_count,
+                    [skew_angle] * string_count,
+                    [random_skew] * string_count,
+                    [blur] * string_count,
+                    [random_blur] * string_count,
+                    [background] * string_count,
+                    [distorsion] * string_count,
+                    [distorsion_orientation] * string_count,
                     [self.handwritten] * string_count,
                     [self.name_format] * string_count,
-                    [self.width] * string_count,
-                    [self.alignment] * string_count,
+                    [width] * string_count,
+                    [alignment] * string_count,
                     [self.text_color] * string_count,
                     [self.orientation] * string_count,
-                    [self.space_width] * string_count )]
-
+                    [space_width] * string_count )]
         
-        self.seq = augmentations
-
-    def __len__(self):
-        'Denotes the total number of samples'
-        return self.batch_size*self.epoch_size
-    def transform(self,img):
-        return self.seq.augment_images(img)
-
-    def __getitem__(self, index):
-        'Generates one sample of data'
-        # Select sample
-        X=self.image_list[index]#/255
-        y=self.strings_int[index]
+        X=image_list[0]
+        y=self.strings_int[0]
+        
         y_len=len(y)
-        
         
         if self.transform== True: 
             X=self.seq.augment_images(X)
-        X=np.squeeze(X)  
-        #X=cv2.cvtColor(X,cv2.COLOR_RGB2GRAY)
-        #X=cv2.cvtColor(X,cv2.COLOR_GRAY2RGB)
-        
-    #    resize_shape=(12,int(12*X.shape[1]/X.shape[0]))
-   #     X = resize(X,resize_shape,mode="constant") 
-#
-#        resize_shape=(32,int(32*X.shape[1]/X.shape[0]))
- #       X = resize(X,resize_shape,mode="constant")     
-  #      
-        X=np.expand_dims(X,0) 
+            
+        #X=np.squeeze(X)   
+
+        #X=np.expand_dims(X,0) 
         X =X/255
         x_len=X.shape[2]
 
